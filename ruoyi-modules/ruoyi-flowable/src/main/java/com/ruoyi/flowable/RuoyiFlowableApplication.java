2e097d1f7d7e4bd277d622d3ceca3836e246711c
li <17337376260@163.com>
2026-03-26 16:39:11 +0800

    3.26新建ruoyi-flowable模块

--- ruoyi-modules/ruoyi-flowable/src/main/java/com/ruoyi/flowable/RuoyiFlowableApplication.java
+++ ruoyi-modules/ruoyi-flowable/src/main/java/com/ruoyi/flowable/RuoyiFlowableApplication.java
@@ -2,6 +2,7 @@ package com.ruoyi.flowable;
 
 import com.ruoyi.common.security.annotation.EnableCustomConfig;
 import com.ruoyi.common.security.annotation.EnableRyFeignClients;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableCustomConfig
 @EnableRyFeignClients
 @SpringBootApplication
+@MapperScan({"com.ruoyi.flowable.mapper", "com.ruoyi.flowable.core.mapper"})
 public class RuoyiFlowableApplication {
     public static void main(String[] args) {
         SpringApplication.run(RuoyiFlowableApplication.class, args);
